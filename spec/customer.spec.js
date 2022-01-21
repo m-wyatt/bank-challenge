1ae1a56a016ca0b5575032b6d2b74b1907251ab5
Matt <wyattmatt@protonmail.com>
2022-01-21 17:09:13 +0000

    Test passing for error if name not string

--- spec/customer.spec.js
+++ spec/customer.spec.js
@@ -12,23 +12,26 @@ describe('Customer Tests:', () => {
 
     })
 
-    it('should have string type customer name property', () => {
-        // Setup
-        const testInput = 1;
-        const testCustomer = new Customer(testInput);
-        // Evaluate
-        const actual = testCustomer.getName();
-        // Verify
-        expect(actual).toBeUndefined();
+    // it('should have string type customer name property', () => {
+    //     // Setup
+    //     const testInput = 1;
+    //     const testCustomer = new Customer(testInput);
+    //     // Evaluate
+    //     const actual = testCustomer.getName();
+    //     // Verify
+    //     expect(actual).toBeUndefined();
 
-    })
+    // })
 
     it('should return error if name argument not string', () => {
         // Setup
         const testInput = 1;
+        // const createCustomerWithNumberName = () => new Customer(1);
+        // Can't do above because throws error before able to reach verify...
+
         // Evaluate
-        const actual = new Customer(testInput);
+
         // Verify
-        expect(function () { actual }).toThrow(new Error('Customer name must be a string.'));
+        expect(function () { new Customer(1) }).toThrow(new Error('Customer name must be a string.'));
     })
 })
--- src/customer.js
+++ src/customer.js
@@ -4,6 +4,8 @@ export class Customer {
     constructor(name) {
         if (typeof name === "string") {
             this.#name = name;
+        } else {
+            throw new Error('Customer name must be a string.');
         }
 
     }
