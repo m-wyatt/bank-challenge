a88f14f7a2cf7c3cbe42f045f4b9ec0fe46b9440
Matt <wyattmatt@protonmail.com>
2022-01-27 14:21:51 +0000

    Failing test for added transaction must be instance of Transaction

--- spec/customer.spec.js
+++ spec/customer.spec.js
@@ -12,200 +12,216 @@ class MockTransactionWithNullNewBalance {
     date = new Date();
     amount = 20;
     newBalance = null;
-    // getAmount = () => this.amount; // don't see how to do it without defining this method
     getAmount = () => { };
     getNewBalance = () => { };
-    // setNewBalance = (newBalance) => { this.newBalance = newBalance };
     setNewBalance = () => { };
-    // How to do it without redefining the methods?
-    // Tried looking at spies but don't see how it would help
 }
 
 
 describe('Customer Tests:', () => {
-    it('should be able to return customer name property with getter', () => {
-        // Setup
-        const testInput = 'George';
-        const testCustomer = new Customer(testInput);
-        const expected = testInput;
-        // Evaluate
-        const actual = testCustomer.getName();
-        // Verify
-        expect(actual).toBe(expected);
 
-    })
+    describe('Customer name property tests: ', () => {
 
-    it('should not be able to return customer name property directly', () => {
-        // Setup
-        const testInput = 'George';
-        const testCustomer = new Customer(testInput);
-        const expected = undefined;
-        // Evaluate
-        const actual = testCustomer.name;
-        // Verify
-        expect(actual).toBe(expected);
+        it('should be able to return customer name property with getter', () => {
+            // Setup
+            const testInput = 'George';
+            const testCustomer = new Customer(testInput);
+            const expected = testInput;
+            // Evaluate
+            const actual = testCustomer.getName();
+            // Verify
+            expect(actual).toBe(expected);
 
-    })
+        });
 
-    // it('should have string type customer name property', () => {
-    //     // Setup
-    //     const testInput = 1;
-    //     const testCustomer = new Customer(testInput);
-    //     // Evaluate
-    //     const actual = testCustomer.getName();
-    //     // Verify
-    //     expect(actual).toBeUndefined();
+        it('should not be able to return customer name property directly', () => {
+            // Setup
+            const testInput = 'George';
+            const testCustomer = new Customer(testInput);
+            const expected = undefined;
+            // Evaluate
+            const actual = testCustomer.name;
+            // Verify
+            expect(actual).toBe(expected);
 
-    // })
+        });
 
-    it('should return error if name argument not string', () => {
-        // Setup
-        const testInput = 1;
-        const expected = new Error('Customer name must be a string.');
-        // const createCustomerWithNumberName = () => new Customer(1);
-        // Can't do above because throws error before able to reach verify...
+        it('should return error if name argument not string', () => {
+            // Setup
+            const testInput = 1;
+            const expected = new Error('Customer name must be a string.');
+            // const createCustomerWithNumberName = () => new Customer(1);
+            // Can't do above because throws error before able to reach verify...
 
-        // Evaluate
+            // Evaluate
 
-        // Verify
-        expect(function () { new Customer(testInput) }).toThrow(expected);
-    })
+            // Verify
+            expect(function () { new Customer(testInput) }).toThrow(expected);
+        });
 
-    it('should be able to return customer balance property with getter', () => {
-        // Setup
-        const testInput = ["John", 10000];
-        const testCustomer = new Customer(...testInput);
-        const expected = testInput[1]
-        // Evaluate
-        const actual = testCustomer.getCurrentBalance();
-        // Verify
-        expect(actual).toBe(expected);
+    });
 
-    })
+    describe('Customer balance property tests: ', () => {
 
-    it('should not be able to return customer balance property directly', () => {
-        // Setup
-        const testInput = ['John', 10000];
-        const testCustomer = new Customer(...testInput);
-        const expected = undefined;
-        // Evaluate
-        const actual = testCustomer.name;
-        // Verify
-        expect(actual).toBe(expected);
+        it('should be able to return customer balance property with getter', () => {
+            // Setup
+            const testInput = ["John", 10000];
+            const testCustomer = new Customer(...testInput);
+            const expected = testInput[1]
+            // Evaluate
+            const actual = testCustomer.getCurrentBalance();
+            // Verify
+            expect(actual).toBe(expected);
 
-    })
+        });
 
-    it('should return error if customer balance argument not number', () => {
-        // Setup
-        const testInput = ["John", "not a number"];
-        const expected = new Error('Current balance must be a number.');
+        it('should not be able to return customer balance property directly', () => {
+            // Setup
+            const testInput = ['John', 10000];
+            const testCustomer = new Customer(...testInput);
+            const expected = undefined;
+            // Evaluate
+            const actual = testCustomer.name;
+            // Verify
+            expect(actual).toBe(expected);
 
-        // Evaluate
+        });
 
-        // Verify
-        expect(function () { new Customer(...testInput) }).toThrow(expected);
-    })
+        it('should return error if customer balance argument not number', () => {
+            // Setup
+            const testInput = ["John", "not a number"];
+            const expected = new Error('Current balance must be a number.');
 
-    it('should have empty default array of transactions', () => {
-        // Setup
-        const testCustomer = new Customer("John", 15000);
-        const expected = 0;
+            // Evaluate
 
-        // Evaluate
-        // This test slightly redundant now as almost identical to "access transactions through getter" below
-        const actual = testCustomer.getTransactions.length;
+            // Verify
+            expect(function () { new Customer(...testInput) }).toThrow(expected);
+        });
 
-        // Verify
-        expect(actual).toBe(expected);
+    });
 
-    })
+    describe('Customer transactions property tests: ', () => {
 
-    it("shouldn't return transactions when accessed directly", () => {
-        // Setup
-        const testCustomer = new Customer("John", 15000);
-        const expected = undefined;
+        it('should have empty default array of transactions', () => {
+            // Setup
+            const testCustomer = new Customer("John", 15000);
+            const expected = 0;
 
-        // Evaluate
-        const actual = testCustomer.transactions;
+            // Evaluate
+            // This test slightly redundant now as almost identical to "access transactions through getter" below
+            const actual = testCustomer.getTransactions.length;
 
-        // Verify
-        expect(actual).toBe(expected);
+            // Verify
+            expect(actual).toBe(expected);
 
-    })
+        });
 
-    it("should return transactions when accessed through getter", () => {
-        // Setup
-        const testCustomer = new Customer("John", 15000);
-        const expected = 0;
+        it("shouldn't return transactions when accessed directly", () => {
+            // Setup
+            const testCustomer = new Customer("John", 15000);
+            const expected = undefined;
 
-        // Evaluate
-        const actual = testCustomer.getTransactions.length;
+            // Evaluate
+            const actual = testCustomer.transactions;
 
-        // Verify
-        expect(actual).toBe(expected);
+            // Verify
+            expect(actual).toBe(expected);
 
-    })
+        });
 
-    it('should add a transaction to transactions array with a method', () => {
-        // Setup
-        const testCustomer = new Customer("James");
-        const testTransaction = new MockTransaction();
-        const expected = testTransaction;
+        it("should return transactions when accessed through getter", () => {
+            // Setup
+            const testCustomer = new Customer("John", 15000);
+            const expected = 0;
 
-        // Evaluate
-        testCustomer.addTransaction(testTransaction);
-        const actual = testCustomer.getTransactions();
+            // Evaluate
+            const actual = testCustomer.getTransactions.length;
 
-        // Verify
-        expect(actual).toContain(expected);
-    })
+            // Verify
+            expect(actual).toBe(expected);
 
-    it('should return new balance when transaction added to transactions array', () => {
-        // Setup
-        const testCustomer = new Customer("James");
-        const testTransaction = new MockTransaction();
-        const getAmountSpy = spyOn(testTransaction, 'getAmount').and.returnValue(20);
-        const expected = testTransaction.amount;
+        });
 
-        // Evaluate
-        const actual = testCustomer.addTransaction(testTransaction);
+        it('should add a transaction to transactions array with a method', () => {
+            // Setup
+            const testCustomer = new Customer("James");
+            const testTransaction = new MockTransaction();
+            const expected = testTransaction;
 
-        // Verify
-        expect(actual).toEqual(expected);
-    })
+            // Evaluate
+            testCustomer.addTransaction(testTransaction);
+            const actual = testCustomer.getTransactions();
 
-    it('should change current balance to new balance when transaction added', () => {
-        // Setup
-        const testCustomer = new Customer("James");
-        const testTransaction = new MockTransaction();
-        const getAmountSpy = spyOn(testTransaction, 'getAmount').and.returnValue(20);
-        const expected = testTransaction.newBalance;
+            // Verify
+            expect(actual).toContain(expected);
+        });
 
-        // Evaluate
-        testCustomer.addTransaction(testTransaction);
-        const actual = testCustomer.getCurrentBalance();
+        it('should not add an element to transactions array if it is not instance of Transaction class', () => {
+            // Setup
+            const testCustomer = new Customer("James");
+            const testTransaction = "20";
+            const expected = new Error('Only instances of Transaction class can be added to customer transactions.')
 
-        // Verify
-        expect(actual).toEqual(expected);
-    })
+            // Execute
+            testCustomer.addTransaction(testTransaction);
 
-    it('should calculate transaction newBalance when adding transaction', () => {
-        // Setup
-        const testTransaction = new MockTransactionWithNullNewBalance();
-        const testCustomer = new Customer('James');
-        const getAmountSpy = spyOn(testTransaction, 'getAmount').and.returnValue(20);
-        const setNewBalanceSpy = spyOn(testTransaction, 'setNewBalance').and.callFake(newBalance => testTransaction.newBalance = newBalance);
-        const expected = 20 // = customer.currentBalance + transaction.amount
+            // Verify
+            expect(testCustomer.getTransactions()).not.toContain(testTransaction);
+            expect(function () { testCustomer.addTransaction(testTransaction) }).toThrow(expected);
 
-        // Evaluate
-        testCustomer.addTransaction(testTransaction);
-        const actual = testTransaction.newBalance;
+        });
 
-        // Verify
-        expect(actual).toEqual(expected);
-        expect(setNewBalanceSpy).toHaveBeenCalledTimes(1);
+
+        it('should return new balance when transaction added to transactions array', () => {
+            // Setup
+            const testCustomer = new Customer("James");
+            const testTransaction = new MockTransaction();
+            const getAmountSpy = spyOn(testTransaction, 'getAmount').and.returnValue(20);
+            const expected = testTransaction.amount;
+
+            // Evaluate
+            const actual = testCustomer.addTransaction(testTransaction);
+
+            // Verify
+            expect(actual).toEqual(expected);
+        });
+
+        it('should change current balance to new balance when transaction added', () => {
+            // Setup
+            const testCustomer = new Customer("James");
+            const testTransaction = new MockTransaction();
+            const getAmountSpy = spyOn(testTransaction, 'getAmount').and.returnValue(20);
+            const expected = testTransaction.newBalance;
+
+            // Evaluate
+            testCustomer.addTransaction(testTransaction);
+            const actual = testCustomer.getCurrentBalance();
+
+            // Verify
+            expect(actual).toEqual(expected);
+        });
+
+        it('should calculate transaction newBalance when adding transaction', () => {
+            // Setup
+            const testTransaction = new MockTransactionWithNullNewBalance();
+            const testCustomer = new Customer('James');
+            const getAmountSpy = spyOn(testTransaction, 'getAmount').and.returnValue(20);
+            const setNewBalanceSpy = spyOn(testTransaction, 'setNewBalance').and.callFake(newBalance => testTransaction.newBalance = newBalance);
+            const expected = 20 // = customer.currentBalance + transaction.amount
+
+            // Evaluate
+            testCustomer.addTransaction(testTransaction);
+            const actual = testTransaction.newBalance;
+
+            // Verify
+            expect(actual).toEqual(expected);
+            expect(setNewBalanceSpy).toHaveBeenCalledTimes(1);
+        });
+
     });
 
     // 1. DO TEST FOR addTransaction() **done**
     // 4. DO TEST FOR transactions is array of Transaction(s)
+    // 5. DO TEST FOR add transaction with negative amount
+
 });
--- spec/transaction.spec.js
+++ spec/transaction.spec.js
@@ -2,134 +2,140 @@ import { Transaction } from '../src/transaction.js';
 
 
 describe('Transaction Tests:', () => {
-    it('should be able to return transaction date property with getter', () => {
-        // Setup
-        const testInput = [new Date(), 5];
-        const testTransaction = new Transaction(...testInput);
-        const expected = testInput[0];
-        // Evaluate
-        const actual = testTransaction.getDate();
-        // Verify
-        expect(actual).toBe(expected);
 
-    })
+    describe('Transaction date property tests: ', () => {
 
-    it('should not be able to return transaction date property directly', () => {
-        // Setup
-        const testInput = [new Date(), 5];
-        const testTransaction = new Transaction(...testInput);
-        const expected = undefined;
-        // Evaluate
-        const actual = testTransaction.date;
-        // Verify
-        expect(actual).toBe(expected);
+        it('should be able to return transaction date property with getter', () => {
+            // Setup
+            const testInput = [new Date(), 5];
+            const testTransaction = new Transaction(...testInput);
+            const expected = testInput[0];
+            // Evaluate
+            const actual = testTransaction.getDate();
+            // Verify
+            expect(actual).toBe(expected);
 
-    })
+        })
+
+        it('should not be able to return transaction date property directly', () => {
+            // Setup
+            const testInput = [new Date(), 5];
+            const testTransaction = new Transaction(...testInput);
+            const expected = undefined;
+            // Evaluate
+            const actual = testTransaction.date;
+            // Verify
+            expect(actual).toBe(expected);
+
+        })
 
 
-    it('should return error if date parameter not instance of Date object', () => {
-        // Setup
-        const testInput = "26/01/22";
-        const expected = new Error('Transaction date must be instance of Date object.');
-        // Evaluate
+        it('should return error if date parameter not instance of Date object', () => {
+            // Setup
+            const testInput = "26/01/22";
+            const expected = new Error('Transaction date must be instance of Date object.');
+            // Evaluate
 
-        // Verify
-        expect(function () { new Transaction(testInput) }).toThrow(expected);
-    })
+            // Verify
+            expect(function () { new Transaction(testInput) }).toThrow(expected);
+        })
 
-    it('should be able to return transaction amount with getter', () => {
-        // Setup
-        const testInput = [new Date(), 10];
-        const testTransaction = new Transaction(...testInput);
-        const expected = testInput[1];
-        // Evaluate
-        const actual = testTransaction.getAmount();
-        // Verify
-        expect(actual).toBe(expected);
-
-    })
-
-    it('should not be able to return transaction amount directly', () => {
-        // Setup
-        const testInput = [new Date(), 10];
-        const testTransaction = new Transaction(...testInput);
-        const expected = undefined
-        // Evaluate
-        const actual = testTransaction.amount;
-        // Verify
-        expect(actual).toBe(expected);
-
-    })
-
-    it('should return error if transaction amount is not a number', () => {
-        // Setup
-        const testInput = [new Date(), "not a number"];
-        const expected = new Error('Transaction amount must be a number.');
-
-        // Evaluate
-
-        // Verify
-        expect(function () { new Transaction(...testInput) }).toThrow(expected);
     });
 
+    describe('Transaction amount property tests: ', () => {
 
+        it('should be able to return transaction amount with getter', () => {
+            // Setup
+            const testInput = [new Date(), 10];
+            const testTransaction = new Transaction(...testInput);
+            const expected = testInput[1];
+            // Evaluate
+            const actual = testTransaction.getAmount();
+            // Verify
+            expect(actual).toBe(expected);
 
-    it('should be able to access newBalance with a getter', () => {
-        // Setup
-        const testTransaction = new Transaction(new Date(), 10);
-        const expected = null;
+        })
 
-        // Evaluate
-        const actual = testTransaction.getNewBalance();
+        it('should not be able to return transaction amount directly', () => {
+            // Setup
+            const testInput = [new Date(), 10];
+            const testTransaction = new Transaction(...testInput);
+            const expected = undefined
+            // Evaluate
+            const actual = testTransaction.amount;
+            // Verify
+            expect(actual).toBe(expected);
 
-        // Verify
-        expect(actual).toEqual(expected);
+        })
+
+        it('should return error if transaction amount is not a number', () => {
+            // Setup
+            const testInput = [new Date(), "not a number"];
+            const expected = new Error('Transaction amount must be a number.');
+
+            // Evaluate
+
+            // Verify
+            expect(function () { new Transaction(...testInput) }).toThrow(expected);
+        });
+
     });
 
-    it('should initialise with null new balance', () => {
-        // Setup
-        const testTransaction = new Transaction(new Date(), 10);
-        const expected = null;
+    describe('Transaction newBalance property tests: ', () => {
 
-        // Evaluate
-        const actual = testTransaction.getNewBalance();
+        it('should be able to access newBalance with a getter', () => {
+            // Setup
+            const testTransaction = new Transaction(new Date(), 10);
+            const expected = null;
 
-        // Verify
-        expect(actual).toEqual(expected);
+            // Evaluate
+            const actual = testTransaction.getNewBalance();
+
+            // Verify
+            expect(actual).toEqual(expected);
+        });
+
+        it('should initialise with null new balance', () => {
+            // Setup
+            const testTransaction = new Transaction(new Date(), 10);
+            const expected = null;
+
+            // Evaluate
+            const actual = testTransaction.getNewBalance();
+
+            // Verify
+            expect(actual).toEqual(expected);
+        });
+
+        it('should not be possible to access newBalance directly', () => {
+            // Setup
+            const testTransaction = new Transaction(new Date(), 10);
+            const expected = undefined;
+
+            // Evaluate
+            const actual = testTransaction.newBalance;
+
+            // Verify
+            expect(actual).toEqual(expected);
+        });
+
+        it('should be able to set newBalance with a setter', () => {
+            // Setup
+            const input = 10;
+            const testTransaction = new Transaction(new Date(), 10);
+            const expected = input;
+
+            // Evaluate
+            testTransaction.setNewBalance(input);
+            const actual = testTransaction.getNewBalance();
+
+            // Verify
+            expect(actual).toEqual(expected);
+        });
+
     });
-
-    it('should not be possible to access newBalance directly', () => {
-        // Setup
-        const testTransaction = new Transaction(new Date(), 10);
-        const expected = undefined;
-
-        // Evaluate
-        const actual = testTransaction.newBalance;
-
-        // Verify
-        expect(actual).toEqual(expected);
-    });
-
-    it('should be able to set newBalance with a setter', () => {
-        // Setup
-        const input = 10;
-        const testTransaction = new Transaction(new Date(), 10);
-        const expected = input;
-
-        // Evaluate
-        testTransaction.setNewBalance(input);
-        const actual = testTransaction.getNewBalance();
-
-        // Verify
-        expect(actual).toEqual(expected);
-    });
-
-
-
-
-
 
     // 2. DO TEST FOR getNewBalance() **done**
-    // 3. DO TEST FOR customer.addTransaction()
+    // 3. DO TEST FOR customer.addTransaction() **done**
 
 });
