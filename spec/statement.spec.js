ce1b8706212b8c23ec18679865a77417d970ff2b
Matt <wyattmatt@protonmail.com>
2022-01-27 16:55:08 +0000

    Statement print transaction failing test added

--- spec/statement.spec.js
+++ spec/statement.spec.js
@@ -14,4 +14,18 @@ describe('Statement Tests: ', () => {
         expect(printHeadingSpy).toHaveBeenCalledOnceWith(input);
         expect(consoleLogSpy).toHaveBeenCalledTimes(1);
     })
+
+    it('should print single transaction with static method', () => {
+        // Setup
+        const printTransactionSpy = spyOn(Statement, 'printTransaction').and.callThrough();
+        const consoleLogSpy = spyOn(console, 'log').and.callThrough();
+        const input = 20;
+
+        // Execute
+        Statement.printTransaction(input);
+
+        // Verify
+        expect(printTransactionSpy).toHaveBeenCalledOnceWith(input);
+        expect(consoleLogSpy).toHaveBeenCalledTimes(1);
+    })
 })
